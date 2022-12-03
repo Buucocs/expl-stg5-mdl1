8b805e58981bb8e91f57293a28e387e67a550d2b
Buucocs <cocaleoo@gmail.com>
2022-12-03 08:11:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Rocketseat Explorer - Stage 02 Projeto Solo
+
+![preview](./github/preview-expl-stg5-mdl1.gif)
+
+Projeto de um jogo da adivinhação.
+
+Projeto desenvolvido dento do curso Explorer da Rocketseat c:
+
+[Clique aqui para acessar](https://buucocs.github.io/expl-stg5-mdl1/)
+
+## Tecnologias
+
+- HTML
+- CSS
+- javaScript
+- Git e Github
+
+## Contato
+
+- contato@leonardohgomes.com.br
+- @leonardohgomes_
+- https://github.com/Buucocs
---
+++ script.js
@@ -0,0 +1,64 @@
+// Variables, Variáveis
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+const inputNumber = document.querySelector('#inputNumber')
+let randomNumber = generateRandomNumber()
+let xAttempts = 0
+
+// Events, Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keypress', keyPressReset)
+inputNumber.focus()
+
+
+// Functions, Funções
+function handleTryClick(event){
+  event.preventDefault()
+  
+  xAttempts++
+
+  if((Number(inputNumber.value) == randomNumber) && xAttempts == 1){
+    screenToggle()
+
+    screen2.querySelector('h2').innerText = `Você acertou em uma única tentativa! ⁠(⁠>⁠o⁠<⁠)⁠`
+
+  } else if(Number(inputNumber.value) == randomNumber){
+    screenToggle()
+
+    screen2.querySelector('h2').innerText = `O número sorteado foi ${randomNumber}.
+    Você acertou em ${xAttempts} tentativas!`
+  } else {printAlert()
+  }
+  inputNumber.value=''
+}
+
+function handleResetClick(){
+  randomNumber = generateRandomNumber()
+  screenToggle()
+  xAttempts = 0
+  inputNumber.focus()
+}
+
+function screenToggle(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+function generateRandomNumber(){
+  return Math.round(Math.random()*10)
+}
+
+function keyPressReset(event){
+  if(event.key=='Enter' && screen1.classList.contains('hide')){
+    handleResetClick()
+  }
+}
+
+function printAlert(){
+  if(Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10){
+    alert('Insira um número entre 0 e 10')
+  }
+}
